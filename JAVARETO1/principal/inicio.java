5c23166f5ea1d6fff4e311f1727bf5ba4e2fb250
JHONSEBASTIANRODRIGUEZBOHADA <104032255+JHONSEBASTIANRODRIGUEZBOHADA@users.noreply.github.com>
2022-11-30 22:14:45 -0500

    Add files via upload

---
+++ JAVARETO1/principal/inicio.java
@@ -0,0 +1,12 @@
+package principal;
+import salud.persona;
+
+public class inicio {
+    public static void main(String[] args) 
+    {
+        persona persona = new persona();
+        persona.pedirDatos();
+        persona.mostrarPersona();
+        persona.calcularImc();
+    }   
+}
